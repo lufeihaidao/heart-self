b1146a4cb9d60dede0dfed185ca4900ba0a82b78
haidao <haidao@server>
2013-04-21 13:16:10 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeartSelf::Application.config.secret_token = '221224234123c418f6924d26a35abbd1b8338c3a6f049e9395aadd614e786294467b81d077046cfb4cfd9f8d4c35ac9ab442505e533018f08cb3bbb0aa313aba'
