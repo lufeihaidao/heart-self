864f4fbe20a954a226ad5d59abfc50d5d57eb758
vagrant <vagrant@rails-starter-box.(none)>
2013-05-09 12:47:05 +0000

    custom to productions new

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -3,8 +3,13 @@ class ProductionsController < ApplicationController
     @productions = Production.all
   end
   def new
-    @user = current_user
-    @p_type =params[:p_type]
-
+    # @user = current_user
+    p_type =params[:p_type]
+    @production = Production.new
+    @production.p_type = p_type
+    if p_type != 'facet' && p_type != 'radial'
+    	redirect_to custom_path
+    end
+    
   end
 end
